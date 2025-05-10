de9df618bf41cecb22343e9cbcdbbb47549f98b2
Azizbek Isroilov <aziz16110904@gmail.com>
2025-05-10 10:34:55 +0500

    goroutine and channels

--- main.go
+++ main.go
@@ -1,24 +1,54 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func printNumbers() {
+func generateNumbers(ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done() // Goroutine tugashini bildiradi
 	for i := 1; i <= 10; i++ {
 		time.Sleep(1 * time.Second)
-		println(i)
+		ch <- i // Goroutine’dan ma'lumotni channel orqali yuborish
 	}
+	close(ch) // Channelni yopamiz, boshqa ma'lumot kelmasligi uchun
 }
-func printLetters() {
-	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+
+func printLetters(ch chan string, wg *sync.WaitGroup) {
+	defer wg.Done() // Goroutine tugashini bildiradi
+	letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 	for _, letter := range letters {
 		time.Sleep(1 * time.Second)
-		println(letter)
+		ch <- letter
 	}
+	close(ch) // Channelni yopamiz
 }
 
 func main() {
-	go printNumbers()
-	go printLetters()
+	var wg sync.WaitGroup // WaitGroup yaratamiz
 
-	time.Sleep(11 * time.Second)
+	ch := make(chan int)     // Channel yaratish
+	ch2 := make(chan string) // Ikkinchi channel
+
+	// Goroutines ishga tushirildi
+	wg.Add(2) // Ikki goroutine qo'shish
+	go generateNumbers(ch, &wg)
+	go printLetters(ch2, &wg)
+
+	// Channel'dan qiymatlarni olish
+	go func() {
+		for num := range ch {
+			fmt.Println(num)
+		}
+	}()
+
+	go func() {
+		for letter := range ch2 {
+			fmt.Println(letter)
+		}
+	}()
+
+	// Goroutines tugashini kutish
+	wg.Wait()
 }
