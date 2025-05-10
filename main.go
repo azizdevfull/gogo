0515c4679d3f5bf2346617f8b46a733f97539d80
Azizbek Isroilov <aziz16110904@gmail.com>
2025-05-10 19:03:47 +0500

    query paramets

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
+		name := c.Query("name")
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pong " + name,
 		})
 	})
 
