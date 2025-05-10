86e424fdc9cf6802b7896ac630b87362bd6537f3
Azizbek Isroilov <aziz16110904@gmail.com>
2025-05-10 17:42:29 +0500

    interfaces

--- main.go
+++ main.go
@@ -1,54 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func generateNumbers(ch chan int, wg *sync.WaitGroup) {
-	defer wg.Done() // Goroutine tugashini bildiradi
-	for i := 1; i <= 10; i++ {
-		time.Sleep(1 * time.Second)
-		ch <- i // Goroutine’dan ma'lumotni channel orqali yuborish
-	}
-	close(ch) // Channelni yopamiz, boshqa ma'lumot kelmasligi uchun
+// interface
+type Notifier interface {
+	Send(message string)
 }
 
-func printLetters(ch chan string, wg *sync.WaitGroup) {
-	defer wg.Done() // Goroutine tugashini bildiradi
-	letters := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
-	for _, letter := range letters {
-		time.Sleep(1 * time.Second)
-		ch <- letter
-	}
-	close(ch) // Channelni yopamiz
+type EmailNotification struct {
+	EmailAddress string
+}
+
+func (e EmailNotification) Send(message string) {
+	fmt.Printf("📧 Emailga [%s] yuborildi: %s\n", e.EmailAddress, message)
+}
+
+type SMSNotification struct {
+	PhoneNumber string
+}
+
+func (s SMSNotification) Send(message string) {
+	fmt.Printf("📱 SMS raqam [%s] ga yuborildi: %s\n", s.PhoneNumber, message)
+}
+
+func sendNotification(n Notifier, msg string) {
+	n.Send(msg)
 }
 
 func main() {
-	var wg sync.WaitGroup // WaitGroup yaratamiz
+	email := EmailNotification{EmailAddress: "user@example.com"}
+	sms := SMSNotification{PhoneNumber: "+998901234567"}
 
-	ch := make(chan int)     // Channel yaratish
-	ch2 := make(chan string) // Ikkinchi channel
-
-	// Goroutines ishga tushirildi
-	wg.Add(2) // Ikki goroutine qo'shish
-	go generateNumbers(ch, &wg)
-	go printLetters(ch2, &wg)
-
-	// Channel'dan qiymatlarni olish
-	go func() {
-		for num := range ch {
-			fmt.Println(num)
-		}
-	}()
-
-	go func() {
-		for letter := range ch2 {
-			fmt.Println(letter)
-		}
-	}()
-
-	// Goroutines tugashini kutish
-	wg.Wait()
+	sendNotification(email, "Salom, bu email xabarnoma!")
+	sendNotification(sms, "Salom, bu SMS xabarnoma!")
 }
