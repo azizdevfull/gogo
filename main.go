bcedb3019db1a904736df9a107c522d2461bbe4a
Azizbek Isroilov <aziz16110904@gmail.com>
2025-05-10 18:12:38 +0500

    User starct and in memory users db

--- main.go
+++ main.go
@@ -10,21 +10,32 @@ func (e AuthError) Error() string {
 	return e.Message
 }
 
-func login(email string, password string) (string, error) {
-	myEmail := "azizdev.full@gmail.com"
-	myPassword := "123456"
-	if myEmail != email || myPassword != password {
-		return "", AuthError{Message: "Email yoki parol xato"}
-	} else {
-		return "Siz muvaffaqiyatli ro'yxatdan o'tdingiz", nil
+type User struct {
+	Name     string
+	Email    string
+	Password string
+}
+
+// In memory userlar bazasi
+var users = []User{
+	{Name: "Azizdev", Email: "azizdev.full@gmail.com", Password: "123456"},
+	{Name: "YoungDev", Email: "young@dev.com", Password: "abcdef"},
+}
+
+func login(email, password string) (User, error) {
+	for _, user := range users {
+		if user.Email == email && user.Password == password {
+			return user, nil
+		}
 	}
+	return User{}, AuthError{Message: "Email yoki parol noto‘g‘ri"}
 }
 
 func main() {
 	user, err := login("azizdev.full@gmail.com", "123456")
 	if err != nil {
-		fmt.Println("Xato: ", err)
-	} else {
-		fmt.Println("Foydalanuvchi: ", user)
+		fmt.Println("Xato:", err)
+		return
 	}
+	fmt.Println("Xush kelibsiz", user.Name)
 }
