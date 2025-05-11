81e6ee6d1d908cfc254833df58969368fa7b75eb
Azizbek Isroilov <aziz16110904@gmail.com>
2025-05-11 20:51:22 +0500

    completed user crud

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -14,6 +14,17 @@ func GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
 
+// find user by id
+func GetUser(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
 func CreateUser(c *gin.Context) {
 	var user models.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -22,4 +33,30 @@ func CreateUser(c *gin.Context) {
 	}
 	database.DB.Create(&user)
 	c.JSON(http.StatusOK, gin.H{"message": "User created successfully", "user": user})
+}
+
+// Update user
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id")
+	var user models.User
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		return
+	}
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	database.DB.Save(&user)
+	c.JSON(http.StatusOK, user)
+}
+
+// Delete user
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+	if err := database.DB.Delete(&models.User{}, id).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Delete failed"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted"})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,7 @@ import (
 func UserRoutes(r *gin.Engine) {
 	r.GET("/users", controllers.GetUsers)
 	r.POST("/users", controllers.CreateUser)
+	r.PUT("/users/:id", controllers.UpdateUser)
+	r.GET("/users/:id", controllers.GetUser)
+	r.DELETE("/users/:id", controllers.DeleteUser)
 }
